2169531872af5cf7ddbd64a59ee8a7ca3260381e
Abdul Muneeb <abdulmuneeb@NX00044.nextbridge.org>
2012-08-28 17:30:13 +0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GridviewDemoStore::Application.config.secret_token = '3e3d4b7162ef2388e1798a731e53f2c7638947343b11b2f2649ef8805785c19370bf975f6d61bcfd4cc1dd5ff77dbfd8ba3529dcdae516a6ff88949a6e65aaec'
